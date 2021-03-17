0c1509c2bc1ac16c529cfcfcb6bbdfd47d9c595b
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-03-17 14:55:19 +0700

    🔨: add adapter for row item

---
+++ app/src/main/java/id/rosyid/githubuser/ui/GithubUserAdapter.kt
@@ -0,0 +1,42 @@
+package id.rosyid.githubuser.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import id.rosyid.githubuser.data.GithubUser
+import id.rosyid.githubuser.databinding.RowItemGithubuserBinding
+
+class GithubUserAdapter(
+    private val listGithubuser: ArrayList<GithubUser>,
+    private val clickListener: (GithubUser) -> View.OnClickListener
+) : RecyclerView.Adapter<GithubUserAdapter.ListViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        return ListViewHolder(
+            RowItemGithubuserBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return listGithubuser.size
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        holder.bind(listGithubuser[position], clickListener)
+    }
+
+    inner class ListViewHolder(
+        val binding: RowItemGithubuserBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(githubUser: GithubUser, clickListener: (GithubUser) -> View.OnClickListener) {
+            // TODO(Use Glide for ImageView)
+            binding.tvName.text = githubUser.name
+            binding.tvUsername.text = githubUser.username
+            binding.baseLayout.setOnClickListener(clickListener(githubUser))
+        }
+    }
+}
