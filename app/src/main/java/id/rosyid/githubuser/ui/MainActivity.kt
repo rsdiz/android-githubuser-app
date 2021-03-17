9fcc896262a00bb91588724dc0c814b2eefe7452
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-03-17 12:33:37 +0700

    ♻️: refactor main activity with view binding

--- app/src/main/java/id/rosyid/githubuser/ui/MainActivity.kt
+++ app/src/main/java/id/rosyid/githubuser/ui/MainActivity.kt
@@ -2,11 +2,15 @@ package id.rosyid.githubuser.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import id.rosyid.githubuser.R
+import id.rosyid.githubuser.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
