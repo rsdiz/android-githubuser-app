b74bf92741e8f19c2e45f6385f093878b625f1fe
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-03-21 21:05:39 +0700

    🔨: initialize data list in activity

--- app/src/main/java/id/rosyid/githubuser/ui/list/MainActivity.kt
+++ app/src/main/java/id/rosyid/githubuser/ui/list/MainActivity.kt
@@ -2,15 +2,25 @@ package id.rosyid.githubuser.ui.list
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import id.rosyid.githubuser.adapter.GithubUserAdapter
+import id.rosyid.githubuser.data.DataList
+import id.rosyid.githubuser.data.GithubUser
 import id.rosyid.githubuser.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var dataList: ArrayList<GithubUser>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        dataList = DataList.getData(this)
+
+        binding.rvGithubUser.layoutManager = LinearLayoutManager(this)
+        // TODO: initialize adapter for recycler view
     }
 }
